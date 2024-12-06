bae2b05cc88a2b3f649ad3a8e96f1d2c94c0826c
Trevon Darby <TJDARBY@email.student.edu>
2024-12-06 18:09:46 -0500

    ..

--- langbite/src/main/java/com/language/App.java
+++ langbite/src/main/java/com/language/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loginScreen"), 600,800);
+        scene = new Scene(loadFXML("profile"), 600,800);
         stage.setScene(scene);
         stage.show();
     }
