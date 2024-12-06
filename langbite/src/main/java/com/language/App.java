77e8dce04f146fe91c3b132391f4829aabf73eef
raswright <rasean0310@gmail.com>
2024-12-06 18:02:39 -0500

    updated backhand

--- langbite/src/main/java/com/language/App.java
+++ langbite/src/main/java/com/language/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("minigameSelect"), 600,800);
+        scene = new Scene(loadFXML("loginScreen"), 600,800);
         stage.setScene(scene);
         stage.show();
     }
